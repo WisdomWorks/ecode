4dbae3ec87bb249fa873e64487db7a18bbe7a134
Van Dao <daovo739@gmail.com>
2023-11-26 18:39:18 +0700

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', '*.config.ts', '*.config.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: true,
+    tsconfigRootDir: __dirname,
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-dupe-args': 'error',
+    'no-dupe-else-if': 'error',
+    'no-dupe-keys': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+
+    //Simple-import-sort
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    //Format
+    'prettier/prettier': 'error',
   },
 }
---
+++ src/ComponentA.tsx
@@ -0,0 +1,5 @@
+export const ComponentA = () => (
+  <div>
+    <h1 className="font-bold text-red-400">test</h1>
+  </div>
+)
