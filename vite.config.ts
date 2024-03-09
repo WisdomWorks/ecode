54a7d6816eb23998caddcbb04c340e2a4d7888d1
Van Dao <daovo739@gmail.com>
2024-03-09 13:47:01 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
@@ -37,6 +37,6 @@ export default defineConfig({
     },
   },
   define: {
-    VITE_API_URL: process.env.VITE_API_URL,
+    VITE_API_URL: loadEnv('', process.cwd()).VITE_API_URL,
   },
 })
